0032ffe6f798351e85a38703ff683fac37825c24
Marco Ancona <marco@morgen.so>
2023-03-14 23:02:52 +0100

    fixes according to review

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Morgen APIs</span>,
@@ -12,6 +14,20 @@ const config: DocsThemeConfig = {
   docsRepositoryBase: 'https://github.com/morgen-so/morgen-dev-docs',
   footer: {
     text: 'Morgen Docs - Morgen AG © 2020 - 2023 All rights reserved.',
+  },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://docs.morgen.so' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+
+    return <>
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={frontMatter.title || 'Morgen API'} />
+      <meta property="og:description" content={frontMatter.description || 'Morgen Developer Documentation'} />
+      <link rel="icon" type="image/x-icon" href="https://uploads-ssl.webflow.com/629614d59c56baedace22d1e/62972f0010f357fdb240353a_favicon.png" />
+    </>
   },
   sidebar: {
     titleComponent({ title, type }) {
